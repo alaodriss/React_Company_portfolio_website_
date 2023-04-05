c2e9004564cdb563a71b770db43a17108f166b79
alaodriss <alaodriss@gmail.com>
2023-04-05 14:30:16 +0200

    add pagination

--- src/ components/Faq/Career.js
+++ src/ components/Faq/Career.js
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from "react";
 import DataInfo from "./DataInfo";
 import "./Career.css";
+import Pagination from "../Pagination/Pagination";
 const Career = () => {
   const url = "https://jsonplaceholder.typicode.com/posts";
 
   const [data, setData] = useState([]);
+
+  // for pagination
+
+  const PER_PAGE = 7;
+  const [currentPage, setCurrentPage] = useState(1);
+  // Invoke when user click to request another page.
+  const handlePageClick = ({selected: selectedPage}) => {
+    setCurrentPage(selectedPage)
+  }
+
+
+  // from an API endpoint with useEffect and useState)
+  const endOffset = currentPage * PER_PAGE ;
+  const currentPageData = data.slice(endOffset, endOffset + PER_PAGE );
+  const pageCount = Math.ceil(data.length / PER_PAGE);
+
 
   const loadPostData = () => {
     fetch(url, {
@@ -12,9 +29,9 @@ const Career = () => {
     })
       .then((res) => res.json())
       .then((result) => {
-        if(result){
-            setData(result)
-            console.log(result);
+        if (result) {
+          setData(result);
+          console.log(result);
         }
       })
       .catch((err) => {
@@ -22,23 +39,30 @@ const Career = () => {
       });
   };
 
-  useEffect(()=> {
-    loadPostData()
-  },[])
+  useEffect(() => {
+    loadPostData();
+  }, []);
 
   return (
-    <div className="container career">
+    <div className="container career" id="career">
       <div className="section_title">
         <h5> Career</h5>
         <span className="line"></span>
       </div>
       <div className="row">
-     {data && data.map((item,index)=>(
-        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12" key={index}>
-                <DataInfo {...item}/>
-            {/* <DataInfo title={item.title} body={item.body}/> */}
-        </div>
-     ))}
+        {data &&
+          currentPageData.map((item, index) => (
+            <div
+              className="col-xl-12 col-lg-12 col-md-12 col-sm-12"
+              key={index}
+            >
+              <DataInfo {...item} />
+              {/* <DataInfo title={item.title} body={item.body}/> */}
+            </div>
+          ))}
+      </div>
+      <div className="pagination-details"> 
+      <Pagination   handlePageClick={handlePageClick} pageCount={pageCount}/>
       </div>
     </div>
   );
